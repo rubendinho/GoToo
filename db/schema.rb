67dae494d6faa852fb8d104d1a9d8e6c2c48f77c
Ruben Izmailyan <ruben@Rubens-MacBook-Pro.local>
2016-04-21 17:49:19 -0500

    scaffold locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160421215217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "city_name"
+    t.string   "state_province"
+    t.integer  "country"
+    t.text     "description"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
 end
