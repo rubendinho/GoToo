4303c23f536f84da38def1979b0b00f618e97963
Jeremy Gunter <gunterja@gmail.com>
2016-06-27 11:50:27 -0500

    Update the landing page. Add date selector to new and edit views for devise. Whitelist the dob in registrations controller.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :username, :hometown, :citizenship, :gender, :dob)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :username, :hometown, :citizenship, :gender, :dob)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :username, :hometown, :citizenship, :gender, :dob)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :username, :hometown, :citizenship, :gender, :dob)
   end
 end
