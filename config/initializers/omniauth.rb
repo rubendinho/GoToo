38980b85ed85b4a5ae50620aefcbe228f0bc9a3a
Jeremy Gunter <gunterja@gmail.com>
2016-07-18 17:26:46 -0700

    Add facebook_app_id and facebook_secret to secrets.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['facebook_app_id'], FACEBOOK_CONFIG['facebook_secret'], {:scope => 'user_about_me'}
+  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'],
+  {:scope => 'user_about_me'}
 end
