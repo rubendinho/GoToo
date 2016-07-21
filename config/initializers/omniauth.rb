33827eb30fb37ed3d3ada247090c6e8b22567d1d
Jeremy Gunter <gunterja@gmail.com>
2016-07-20 17:08:09 -0700

    Comment out a possible conflict in the omniauth initializer.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'],
-  {:scope => 'public_profile,email'}
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'],
+#   {:scope => 'public_profile,email'}
+# end
