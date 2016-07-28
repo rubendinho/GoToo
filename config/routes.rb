b9c2e906bdcb99dcdca4c307a959d8290d1f1cb5
Jeremy Gunter <gunterja@gmail.com>
2016-07-28 15:04:33 -0700

    Create a FormUser model. Update routes to use FormUser model for devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :attractions
   devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, class_name: 'FormUser'
   resources :users
   resources :locations
   root 'welcome#index'
