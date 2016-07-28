4c20a75d80078ac593c3de5761f9985124808d91
Jeremy Gunter <gunterja@gmail.com>
2016-07-28 14:55:46 -0700

    Update user model to use omniauth and remove :validatable(for now). Add config settings for facebook to devise initializer.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :confirmable, :omniauthable
 
    def full_name
      "#{self.first_name} #{self.last_name}"
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,7 +239,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], scope: "email"
+  # config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
